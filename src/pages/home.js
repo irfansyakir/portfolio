52106035b5a8eb947c5369711a4bb1ccae9b8d75
irfansyakir <46922818+irfansyakir@users.noreply.github.com>
2025-04-17 11:42:59 +0800

    remove react-router-dom references

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import './home.css';
 import profileImage from '../assets/images/profile.jpg'; 
 
