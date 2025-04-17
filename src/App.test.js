8ba3fc61ec1207f7c39947ccf89d39fb6dbae7e9
irfansyakir <46922818+irfansyakir@users.noreply.github.com>
2025-04-17 11:41:11 +0800

    text encoder tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,13 @@ import App from './App';
 import { TextDecoder, TextEncoder } from "text-encoding";
 
 
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder;
+}
+
+if (typeof global.TextDecoder === 'undefined') {
+  global.TextDecoder = TextDecoder;
+}
 
 test('renders learn react link', () => {
   render(<App />);
